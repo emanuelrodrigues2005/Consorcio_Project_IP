f87bc854cdd1a8886c72940f766fce45e0ba5a16
Emanuel Rodrigues <emanuelrodrigues112358@gmail.com>
2025-02-06 01:15:49 -0300

    feat: Comentários de Exceções Adicionado

--- src/main/java/br/ufrpe/ip/projeto/controllers/BoletoController.java
+++ src/main/java/br/ufrpe/ip/projeto/controllers/BoletoController.java
@@ -26,35 +26,35 @@ public class BoletoController {
 
     public ArrayList<Boleto> getAllBoletos() {
         return this.repositoryBoleto.getAllBoletos();
-    }
+    } //exceptions: ArrayVazio
 
     public int getIdBoleto(Contrato contratoBoleto, int numeroParcela) {
         return this.repositoryBoleto.getIdBoleto(contratoBoleto, numeroParcela);
-    }
+    } //exceptions: IdBoletoInexistente, ContratoInexistente, CampoInvalido
 
     public Boleto getBoletoById(int idBoleto) {
         return this.repositoryBoleto.getBoletoById(idBoleto);
-    }
+    } //exceptions: BoletoInexistente, IdBoletoInexistente, CampoInvalido
 
     public Boleto getBoletoByContrato(Contrato contrato) {
         return this.repositoryBoleto.getBoletoByContrato(contrato);
-    }
+    } //exceptions: BoletoInexistente, ContratoInexistente, CampoInvalido
 
     public void createBoleto(Contrato contratoBoleto, LocalDate dataVencimento, int numeroParcela) {
         this.repositoryBoleto.createBoleto(contratoBoleto, dataVencimento, numeroParcela);
-    }
+    } //exceptions: ContratoInexistente, CampoInvalido
 
     public void updateDataPagamento(Boleto boleto) {
         this.repositoryBoleto.updateDataPagamento(boleto);
-    }
+    } //exceptions: BoletoInexistente, CampoInvalido
 
     public void updateStatusBoleto(Boleto boleto, StatusBoletoEnum statusBoleto) {
         this.repositoryBoleto.updateStatusBoleto(boleto, statusBoleto);
-    }
+    } //exceptions: BoletoInexistente, CampoInvalido
 
-    public void deleteBoletoById(int idBoleto) {
+    public void deleteBoleto(int idBoleto) {
         this.repositoryBoleto.deleteBoleto(idBoleto);
-    }
+    } //exceptions: BoletoInexistente, CampoInvalido
 
     public void atualizarBoletoVencido(Boleto boleto) {
         if(boleto.getStatusBoleto() == StatusBoletoEnum.PENDENTE && LocalDate.now().isAfter(boleto.getDataVencimento())) {
@@ -62,10 +62,10 @@ public class BoletoController {
 
             boleto.setValorBoleto(boleto.getValorBoleto() + (boleto.getValorBoleto() * 0.05));
         }
-    }
+    } //exceptions: BoletoInexistente
 
     public void verificarBoletosVencidos() {
-        for(Boleto boleto : this.repositoryBoleto.getAllBoletos()) {
+        for (Boleto boleto : this.repositoryBoleto.getAllBoletos()) {
             atualizarBoletoVencido(boleto);
         }
     }
@@ -77,5 +77,5 @@ public class BoletoController {
             this.updateDataPagamento(boleto);
             this.updateStatusBoleto(boleto, StatusBoletoEnum.PAGO);
         }
-    }
+    } //exceptions: BoletoInexistente
 }
